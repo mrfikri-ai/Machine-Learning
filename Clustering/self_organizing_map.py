6bcf3b75731942673522ce4a04ce97b5aeff2c1d
Muhamad Rausyan Fikri <9569417+mrfikri-ai@users.noreply.github.com>
2023-06-09 09:27:51 +0300

    Update self_organizing_map.py

--- Clustering/self_organizing_map.py
+++ Clustering/self_organizing_map.py
@@ -1,74 +1,112 @@
-# TODO: Fix the error in the self organizing maps
-
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from scipy.spatial.distance import cdist
 
-class SOM:
-    def __init__(self, input_dim, output_dim, lr=0.1, sigma=None):
-        self.input_dim = input_dim
-        self.output_dim = output_dim
-        self.lr = lr
-        self.sigma = sigma if sigma is not None else np.sqrt(input_dim**2 + output_dim**2)/2
-        
-        self.weights = np.random.rand(output_dim[0], output_dim[1], input_dim[0], input_dim[1])
-        
-    def get_bmu(self, input_data):
-        dist = np.sum((self.weights - input_data)**2, axis=(2,3))
-        return np.unravel_index(np.argmin(dist), dist.shape)
-    
-    def update_weights(self, bmu, input_data, epoch):
-        lr_decay = 1 - epoch / n_epochs
-        sigma_decay = self.sigma * lr_decay
-        lr_cur = self.lr * lr_decay
-        for i in range(self.output_dim[0]):
-            for j in range(self.output_dim[1]):
-                dist = np.sqrt((i-bmu[0])**2 + (j-bmu[1])**2)
-                if dist <= sigma_decay:
-                    influence = np.exp(-dist**2 / (2*sigma_decay**2))
-                    self.weights[i][j] += lr_cur * influence * (input_data - self.weights[i][j])
-                    
-    def train(self, data, n_epochs):
-        for epoch in range(n_epochs):
-            for d in data:
-                bmu = self.get_bmu(d)
-                self.update_weights(bmu, d, epoch)
-                
-    def predict(self, data):
-        predictions = []
-        for d in data:
-            bmu = self.get_bmu(d)
-            predictions.append(bmu)
-        
-        labels = np.zeros(len(data), dtype=np.int)
-        centroids = np.zeros((self.output_dim[0]*self.output_dim[1], self.input_dim[0]*self.input_dim[1]))
-        
-        for i, p in enumerate(predictions):
-            labels[i] = p[0] * self.output_dim[1] + p[1]
-            centroids[labels[i]] += data[i]
-        
-        for i in range(len(centroids)):
-            if np.sum(labels == i) > 0:
-                centroids[i] /= np.sum(labels == i)
-        
-        return labels, centroids
-        
-    def plot_weights(self):
-        plt.imshow(np.vstack([np.hstack(self.weights[i,j]) for i in range(self.output_dim[0]) for j in range(self.output_dim[1])]))
-        plt.axis('off')
-        plt.show()
+def initialize_weights(input_dim, output_dim):
+    """
+    Initialize the weights for the SOM.
 
+    Parameters:
+    - input_dim: Number of input features.
+    - output_dim: Shape of the SOM grid.
 
-# Generate sample data
-data = np.random.rand(100, 2)
+    Returns:
+    - weights: Initial weight matrix of shape (output_dim[0], output_dim[1], input_dim).
+    """
+    weights = np.random.randn(output_dim[0], output_dim[1], input_dim)
+    return weights
 
-# Create and train the SOM
-som = SOM(input_dim=(2,), output_dim=(10, 10), lr=0.1)
-som.train(data, n_epochs=100)
+def find_best_matching_unit(sample, weights):
+    """
+    Find the best matching unit (BMU) for a given sample.
 
-# Get the labels and centroids
-labels, centroids = som.predict(data)
+    Parameters:
+    - sample: Input sample to be matched.
+    - weights: SOM weight matrix.
 
-# Print the labels and centroids
-print(labels)
-print(centroids)
+    Returns:
+    - bmu_index: Indices of the BMU in the weight matrix.
+    """
+    distances = cdist(sample[np.newaxis, :], weights.reshape(-1, weights.shape[2]))
+    bmu_index = np.argmin(distances)
+    return np.unravel_index(bmu_index, weights.shape[:2])
 
+def update_weights(sample, weights, bmu_index, learning_rate, radius):
+    """
+    Update the weights of the SOM based on the best matching unit (BMU).
+
+    Parameters:
+    - sample: Input sample.
+    - weights: SOM weight matrix.
+    - bmu_index: Indices of the BMU in the weight matrix.
+    - learning_rate: Learning rate for weight update.
+    - radius: Neighborhood radius for weight update.
+    """
+    rows, cols = weights.shape[:2]
+    bmu_row, bmu_col = bmu_index
+
+    for i in range(rows):
+        for j in range(cols):
+            dist = np.sqrt((i - bmu_row) ** 2 + (j - bmu_col) ** 2)
+            if dist <= radius:
+                influence = np.exp(-dist / (2 * (radius ** 2)))
+                weights[i, j] += learning_rate * influence * (sample - weights[i, j])
+
+def self_organizing_maps(X, output_dim, num_epochs, learning_rate_initial, radius_initial):
+    """
+    Perform self-organizing maps (SOM) clustering.
+
+    Parameters:
+    - X: Input data array of shape (n_samples, n_features).
+    - output_dim: Shape of the SOM grid.
+    - num_epochs: Number of training epochs.
+    - learning_rate_initial: Initial learning rate.
+    - radius_initial: Initial neighborhood radius.
+
+    Returns:
+    - weights: Final SOM weight matrix.
+    - cluster_labels: Cluster labels for each input sample.
+    """
+    n_samples, n_features = X.shape
+    weights = initialize_weights(n_features, output_dim)
+    learning_rate = learning_rate_initial
+    radius = radius_initial
+
+    for epoch in range(num_epochs):
+        for sample in X:
+            bmu_index = find_best_matching_unit(sample, weights)
+            update_weights(sample, weights, bmu_index, learning_rate, radius)
+
+        # Decay learning rate and radius
+        learning_rate *= 0.9
+        radius *= 0.9
+
+    # Assign cluster labels based on the BMU for each sample
+    cluster_labels = np.zeros(n_samples, dtype=int)
+    for i, sample in enumerate(X):
+        bmu_index = find_best_matching_unit(sample, weights)
+        cluster_labels[i] = np.ravel_multi_index(bmu_index, output_dim)
+
+    return weights, cluster_labels
+
+# Generate sample data using make_blobs
+np.random.seed(42)
+X, _ = make_blobs(n_samples=200, centers=4, n_features=2, random_state=42)
+
+# Set SOM parameters
+output_dim = (10, 10)  # Shape of the SOM grid
+num_epochs = 100  # Number of training epochs
+learning_rate_initial = 0.5  # Initial learning rate
+radius_initial = max(output_dim) / 2  # Initial neighborhood radius
+
+# Perform self-organizing maps clustering
+weights, cluster_labels = self_organizing_maps(X, output_dim, num_epochs, learning_rate_initial, radius_initial)
+
+# Plot the clusters and SOM grid
+plt.scatter(X[:, 0], X[:, 1], c=cluster_labels, cmap='viridis')
+plt.scatter(weights[:, :, 0], weights[:, :, 1], marker='x', color='red')
+plt.title('Self-Organizing Maps Clustering')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
